5b6c6e251926c222d1068c4073fcce1582da6502
User <User@DESKTOP-G0A3IIV>
2024-04-23 01:28:17 -0300

    Adicionar arquivos de projeto.

---
+++ lista1exe9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exe9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o diâmetro do circulo:");
+            double diametro = Convert.ToDouble(Console.ReadLine());
+
+            double raio = diametro / 2;
+            double area = Math.PI * Math.Pow(raio, 2);
+
+            Console.WriteLine($"A area do círculo é: {area}");
+        }
+    }
+}
